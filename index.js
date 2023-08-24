bd9b37fccd24114ed0d7e88188c93bbf8926f7e6
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-08-24 17:02:56 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+// this is a js file
+// Enzo Eduardo
+
+//exercicio de interpretação de código A
+
+//resposta 1. a. false b. false c. true d. boolean
+
+//resposta 2. Esqueceu de dar console.log com a primeira variavel + a segunda 
+
+//resposta 3. o codigo não funciona pois não se deve colocar consolo.log(SOMA) e sim console.log(primeiroNumero + segundoNumero)
+
+//exercicio de escrita de código B
+//exercicio 1.
+/*
+let idade = Number(prompt("qual a sua idade?",));
+console.log("você tem", idade);
+let perguntarBest = Number(prompt("quantos anos seu best tem?");
+console.log("seu best tem", perguntarBest); 
+
+let idadeBest = 16;
+let minhaIdade = 16;
+console.log("você tem a mesma idade que seu best?", minhaIdade > idadeBest);
+*/
+
+//exercicio 2 
+/*
+let numeroPar = 10;
+let resposta = Number(prompt("bote o seu numero par");
+console.log(numeroPar % 2);
+*/
+/* o resultado sempre sera 0 independente do numero colocado*/
+/*o resuoltado sera 1 independente do numero*/
+//exercicio 3
+/*
+let anosIdade = prompt("digite sua idade em anos"); 
+let anos = ("qual a sua idade em anos?", anosIdade);
+console.log("quantos anos você tem?", anosIdade)
+let idadeM = Number(anosIdade * 12);
+console.log("idade em meses", idadeM)  
+let idadeDias = Number(365 * idadeM);
+console.log("idade em dias", idadeDias); 
+let IdadeH = Number(idadeDias * 24);
+console.log("sua idade em horas", IdadeH);
+*/
+//exercicio 4
+/*
+let numero = 24;
+let number = 14;
+
+let primeiroNumero = prompt("digite um numero");
+let segundoNumero = prompt("digite um segundo numero");
+console.log("você digitou", primeiroNumero);
+console.log("você digitou", segundoNumero);
+
+console.log("o primeiro numero é maior que segundo?", primeiroNumero > segundoNumero);
+console.log("o primeiro numero é igual ao segundo?", primeiroNumero <= segundoNumero);
+console.log("primeiro numero é divisivel pelo sendo?", Boolean(primeiroNumero / segundoNumero));
+console.log("o segundo numero é divisivel oelo primeiro?",Boolean (segundoNumero / primeiroNumero));
+*/
